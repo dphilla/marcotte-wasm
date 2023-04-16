5252fdeac5577030156a002a4b606cb49b296890
Daniel Phillips <daniel@loopholelabs.io>
2023-04-16 23:50:35 +0200

    check in deleted files

--- libc_read.rs
+++
@@ -1,4 +0,0 @@
-// reads data from a file descriptor into a buffer
-pub extern "C" fn read(fd: c_int, buf: *mut c_void, count: size_t) -> ssize_t {
-    {{{read_body}}}
-}
--- libc_write.rs
+++
@@ -1,4 +0,0 @@
-// writes data from a buffer to a file descriptor
-pub extern "C" fn write(fd: c_int, buf: *const c_void, count: size_t) -> ssize_t {
-    {{{write_body}}}
-}
